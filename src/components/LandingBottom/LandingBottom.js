572527a31343f040ae9088a9466a81816c89ee7d
oluwadareseyi <oluwadareseyii@gmail.com>
2020-06-23 09:42:06 +0100

    finish progress section

--- src/components/LandingBottom/LandingBottom.js
+++ src/components/LandingBottom/LandingBottom.js
@@ -1,11 +1,52 @@
 import React from "react";
 import Upload from "./Upload/Upload";
+import "./LandingBottom.scss";
 // import ProductSlider from "./productSlider/ProductSlider";
 
 const LandingBottom = () => {
   return (
     <div>
       <Upload />
+      <div className="progress-con">
+        <div className="content">
+          <div className="left">
+            <div className="topic">Our Progress</div>
+            <div className="title">
+              Successful partnership with great <span>brands</span>
+            </div>
+            <div className="sub">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor
+              <span>
+                invidunt ut labore et dolore magna aliquyam erat, sed diam
+                voluptua. At vero eos et
+              </span>
+            </div>
+            <div className="stats">
+              <div className="years">
+                <div className="number">10+</div>
+                <div className="text">Years Of Operation</div>
+              </div>
+
+              <div className="years">
+                <div className="number">120+</div>
+                <div className="text">Successful Projects</div>
+              </div>
+
+              <div className="years">
+                <div className="number">98+</div>
+                <div className="text">Happy Clients</div>
+              </div>
+            </div>
+            <button>Know More</button>
+          </div>
+          <img
+            src={require("../../assets/images/progress.jpg")}
+            className="right"
+            alt="progress"
+          />
+        </div>
+      </div>
     </div>
   );
 };
