e9267ddf56daea24fdd4b7459a208e7c5271331b
oluwadareseyi <oluwadareseyii@gmail.com>
2020-07-06 10:33:51 +0100

    added slick package

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 import Landing from "./containers/Landing/Landing";
+import SignUp from "./containers/SignUp/SignUp";
 
 const App = () => {
   return (
-    <div className="App">
-      <Landing />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Landing} />
+          <Route path="/create" exact component={SignUp} />
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
---
+++ src/containers/SignUp/SignUp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SignUp.scss";
+
+const SignUp = () => {
+  return (
+    <div>
+      <div>Sign Up page</div>
+    </div>
+  );
+};
+
+export default SignUp;
